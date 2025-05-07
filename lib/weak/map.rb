254fa8430e01a2bfe96078d700c72396c8f5a485
Holger Just <git@holgerjust.de>
2025-05-07 21:40:38 +0200

    Consistently use require rather than require_relative everywhere

--- lib/weak/map.rb
+++ lib/weak/map.rb
@@ -5,11 +5,10 @@
 # This software may be modified and distributed under the terms
 # of the MIT license. See the LICENSE.txt file for details.
 
-require_relative "map/weak_keys_with_delete"
-require_relative "map/weak_keys"
-require_relative "map/strong_keys"
-require_relative "map/strong_secondary_keys"
-
+require "weak/map/weak_keys_with_delete"
+require "weak/map/weak_keys"
+require "weak/map/strong_keys"
+require "weak/map/strong_secondary_keys"
 require "weak/undefined"
 
 ##
