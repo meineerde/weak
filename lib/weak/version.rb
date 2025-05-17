1f13e8c3c9ccfceed1782f7745277b81ce45d5fc
Holger Just <git@holgerjust.de>
2025-05-17 13:40:23 +0200

    Add more convenient version API

--- lib/weak/version.rb
+++ lib/weak/version.rb
@@ -7,8 +7,36 @@
 
 ##
 module Weak
-  # The {Weak} version as a `Gem::Version` string. We follow semantic
-  # versioning.
-  # @see https://semver.org/
-  VERSION = "0.2.0"
+  # Version information about {Weak}}. We follow semantic versioning.
+  module Version
+    # MAJOR version. It is incremented after incompatible API changes
+    MAJOR = 0
+    # MINOR version. It is incremented after adding functionality in a
+    # backwards-compatible manner
+    MINOR = 2
+    # PATCH version. It is incremented when making backwards-compatible
+    # bug-fixes.
+    PATCH = 0
+    # PRERELEASE suffix. Set to a alphanumeric string on any pre-release
+    # versions like beta or RC releases; `nil` on regular releases
+    PRERELEASE = nil
+
+    # The {Weak} version as a `Gem::Version` string. We follow semantic
+    # versioning.
+    # @see https://semver.org/
+    STRING = [MAJOR, MINOR, PATCH, PRERELEASE].compact.join(".").freeze
+
+    # @return [Gem::Version] the {Weak} version as a `Gem::Version` object
+    def self.gem_version
+      Gem::Version.new STRING
+    end
+
+    # @return [String] the Weak version as a `Gem::Version` string
+    def self.to_s
+      STRING
+    end
+  end
+
+  # (see Version::STRING)
+  VERSION = Weak::Version::STRING
 end
--- spec/weak/version_spec.rb
+++ spec/weak/version_spec.rb
@@ -8,11 +8,49 @@
 require "spec_helper"
 require "weak/version"
 
-RSpec.describe "Weak::VERSION" do
-  it "is a version number" do
-    expect(Weak::VERSION)
+RSpec.describe "Weak::Version" do
+  it "defines a major version" do
+    expect(Weak::Version::MAJOR).to be_a(Integer).and be >= 0
+  end
+
+  it "defines a minor version" do
+    expect(Weak::Version::MINOR).to be_a(Integer).and be >= 0
+  end
+
+  it "defines a patch version" do
+    expect(Weak::Version::PATCH).to be_a(Integer).and be >= 0
+  end
+
+  it "may define a valid prerelease version" do
+    expect(Weak::Version::PRERELEASE)
+      .to be_nil
+      .or be_a(String).and be_frozen.and match(/\A[a-z0-9_-]+\z/)
+  end
+
+  it "has a version number" do
+    expect(Weak::Version::STRING)
       .to be_a(String)
       .and be_frozen
       .and match %r{\A\d+(?:\.\d+)+(?:\.[a-z][a-z0-9_]+)?\z}i
+  end
+
+  describe ".to_s" do
+    it "equals Weak::Version::STRING" do
+      expect(Weak::Version.to_s).to equal Weak::Version::STRING
+    end
+  end
+
+  describe ".gem_version" do
+    it "is a Gem::Version" do
+      expect(Weak::Version.gem_version).to be_a Gem::Version
+    end
+
+    it "matches Weak::Version::STRING" do
+      expect(Weak::Version.gem_version.to_s).to eq Weak::Version::STRING
+    end
+  end
+
+  it "exposes the version as Weak::VERSION" do
+    expect(Weak::VERSION).to equal Weak::Version::STRING
   end
 end
